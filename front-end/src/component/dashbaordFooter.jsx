5920047795b53939d916d4fb441bbc1d40a03689
Ehiz <iseghohimhene@gmail.com>
2025-08-02 23:16:19 +0100

    yes

--- front-end/src/component/dashbaordFooter.jsx
+++ front-end/src/component/dashbaordFooter.jsx
@@ -2,6 +2,7 @@ import '../css/dashboardCss/dashboard.css'
 export const DashboardFooter = () =>{
   return(
     <div>
+      
       {/* <div className='dashboard-footer'>
         <p>Copyright <i class="bi bi-c-circle sm-text-2"></i> 2025 Dashboard. All rights reserved.</p>
       </div> */}
